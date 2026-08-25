210627a449a4ac4b97c4d90abfa97964c6012474
Sinryou <li_yunghsing@outlook.com>
2026-08-25 22:29:07 +0800

    v1.1.1

--- main.py
+++ main.py
@@ -2193,7 +2193,7 @@ class MainWindow(QtWidgets.QWidget):
             "基于 PyQt6 + aapt2 的图形化<br>"
             "解析 APK 文件信息的工具程序<br><br>"
             '更多信息: <a href="https://github.com/Sinryou/WinApkInfo">项目主页</a><br>'
-            "版本: 1.1.0<br>"
+            "版本: 1.1.1<br>"
             "Copyright (c) 2025-2026 Sinryou.<br>At MIT License."
         )
 
